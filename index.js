3d897f99e3b3554f07ba8947fc2467da5b98df17
Hans Naert <hans.naert@vives.be>
2023-10-04 20:18:06 +0200

    map observable

--- index.js
+++ index.js
@@ -1,14 +1,17 @@
-var Rx = require('rxjs');
+var Observable = require('rxjs').Observable;
+var map = require('rxjs/operators').map;
 
-var observable = new Rx.Observable(function(observer) { 
+var observable = new Observable(function(observer) { 
 	observer.next(1); 
 	observer.next(2); 
 	observer.next(3); 
 	setTimeout(() => { observer.next(4); observer.complete(); }, 1000); 
 });
 
+var observable_byTen = observable.pipe(map(x => 10 * x));
+
 console.log('just before subscribe'); 
-observable.subscribe({ 
+observable_byTen.subscribe({ 
 	next: x => console.log('got value ' + x), 
 	error: err => console.error('something wrong occurred: '+err), 
 	complete: () => console.log('done'), }); 
